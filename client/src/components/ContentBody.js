7056cd3e2d10f107062e9809e913cc451c7452de
ramla199 <ramlush143@gmail.com>
2022-05-24 23:38:30 +0100

    200-2

--- client/src/components/ContentBody.js
+++ client/src/components/ContentBody.js
@@ -1,7 +1,11 @@
 import AddVideo from "./AddVideo";
+import VideoSection from "./VideoSection";
 const ContentBody = () => {
     return (<div className="content-body">
         <AddVideo />
+        <div className="video-list">
+            <VideoSection />
+        </div>
     </div>
     );
 }
---
+++ client/src/components/VideoSection.js
@@ -0,0 +1,14 @@
+const VideoSection = () => {
+    return (<div className="vid-wrapper">
+        <h4>hi</h4>
+        <div className="icon-wrapper">
+            <span className="up-icon">hi</span>
+            <span className="count-vote">up</span>
+            <span className="down-icon">down</span>
+        </div>
+        <div className="video-container">vid</div>
+        <button className="btn btn-delete">Delete</button>
+    </div>);
+}
+
+export default VideoSection;
